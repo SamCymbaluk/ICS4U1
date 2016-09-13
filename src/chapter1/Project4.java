97bdd1ac123a72e513272cc7c42d85f1fee6ad72
fernandky728 <fernandky728@010-308-02285.hwcdsb.ca>
2016-09-13 11:35:23 -0400

    Added Project4 to chapter 1.

---
+++ src/chapter1/Project4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter1;
+
+import util.IO;
+
+/**
+ *
+ * @author fernandky728
+ */
+public class Project4 {
+    public static void main  (String[] args ) {
+        double pound = IO.readDouble("Enter weight (lbs)"); 
+        double kilo = pound / 2.2;
+        IO.showMessage("Converted weight (kgs)" + kilo );
+        
+    
+    
+    
+    
+    }
+    
+}
