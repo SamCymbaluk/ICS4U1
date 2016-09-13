a7c0fc965299aa0985a2ffab97a77e267719d5c5
cymbalusa405 <cymbalusa405@010-308-02290.hwcdsb.ca>
2016-09-13 12:20:16 -0400

    Setup main class

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import chapter1.Assignment8;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cymbalusa405
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        Assignment8.main(args);
+    }
+}
