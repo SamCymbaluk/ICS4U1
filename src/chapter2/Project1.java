9ce5922d54bba5af79183996865796bd8a894a53
cymbalusa405 <cymbalusa405@010-308-02290.hwcdsb.ca>
2016-09-14 11:42:40 -0400

    Added Chapter 2 Project 1

---
+++ src/chapter2/Project1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter2;
+
+import util.IO;
+
+/**
+ *
+ * @author cymbalusa405
+ */
+public class Project1 {
+    public static void main(String[] args){
+        double score = IO.readDouble("Enter a score bewtween 0.0 and 100.0");
+        
+        boolean b = (50 > 0)||(50 == 10);
+ 
+        
+        if(score > 65.0){
+            IO.showMessage("Passing"); 
+        }else{
+            IO.showMessage("Failing");
+        }
+        
+       
+    }
+}
