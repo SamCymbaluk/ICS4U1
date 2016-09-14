d877b0c670d0fd79416ef0ae589eb83a2368548c
fernandky728 <fernandky728@010-308-02285.hwcdsb.ca>
2016-09-14 11:44:24 -0400

    Added Project2 to chapter 2.

---
+++ src/chapter2/Project2.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter2;
+
+/**
+ *
+ * @author fernandky728
+ */
+public class Project2 {
+    
+}
