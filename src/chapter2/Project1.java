9783466562e5ddf287d8e6fcbb08ef44f8a640e4
fernandky728 <fernandky728@010-308-02285.hwcdsb.ca>
2016-09-15 12:10:15 -0400

    Added Project2 to chapter 2.

--- src/chapter2/Project1.java
+++ src/chapter2/Project1.java
@@ -10,7 +10,7 @@ import util.IO;
  *
  * @author cymbalusa405
  */
-public class Project1 {
+public class Project1 { 
     public static void main(String[] args){
         double score = IO.readDouble("Enter a score bewtween 0.0 and 100.0");
         
@@ -22,7 +22,7 @@ public class Project1 {
         }else{
             IO.showMessage("Failing");
         }
-        
+      
        
     }
 }
--- src/chapter2/Project2.java
+++ src/chapter2/Project2.java
@@ -4,10 +4,30 @@
  */
 package chapter2;
 
+import util.IO;
+
 /**
  *
  * @author fernandky728
  */
 public class Project2 {
     
+        public static void main(String[] args){
+        double score = IO.readDouble("Enter a score bewtween 0.0 and 100.0");
+        
+        while (!(score < 0) ){          
+            if(score > 65.0){
+                IO.showMessage("Passing"); 
+            }else{
+                IO.showMessage("Failing");
+            }
+             score = IO.readDouble("Enter a score bewtween 0.0 and 100.0");
+        } 
+      
+      
+   
+      
+       
+    }
 }
+
